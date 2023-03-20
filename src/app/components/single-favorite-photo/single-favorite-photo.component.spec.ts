0e1e3faaf39fb0844f41f00d5d186568f4ae9a3f
Maksim Nikolski <nikolskijmaksim@gmail.com>
2023-03-20 02:59:20 +0100

    Added unit tests

--- src/app/components/single-favorite-photo/single-favorite-photo.component.spec.ts
+++ src/app/components/single-favorite-photo/single-favorite-photo.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SingleFavoritePhotoComponent } from './single-favorite-photo.component';
+import {ActivatedRoute} from "@angular/router";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {FavoritePhotosService} from "../../services/favorite-photos.service";
 
-describe('SingleImageDetailsComponent', () => {
+describe('SingleFavoritePhotoComponent', () => {
   let component: SingleFavoritePhotoComponent;
   let fixture: ComponentFixture<SingleFavoritePhotoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SingleFavoritePhotoComponent ]
+      declarations: [ SingleFavoritePhotoComponent ],
+      providers: [
+        FavoritePhotosService,
+        {
+        provide: ActivatedRoute, useValue: {
+          snapshot: {
+            paramMap: {
+              get: () => 'id1'
+            }
+          }
+        }
+      }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
 
@@ -19,5 +34,20 @@ describe('SingleImageDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should find proper image by id', () => {
+    component['favoriteImagesService'].addPhotoToFavorites({id: 'id1', imgSrc: 'src1', addedToFavorites: true});
+    component['favoriteImagesService'].addPhotoToFavorites({id: 'id2', imgSrc: 'src2', addedToFavorites: true});
+
+    const imgSrcSpy = spyOn(component.imgSrc$, 'next');
+
+    component.ngOnInit();
+
+    expect(imgSrcSpy).toHaveBeenCalledWith('src1');
+  });
+
+  afterEach(() => {
+    window.localStorage.clear();
   });
 });
